1909fc8d48e2249850453f412fc42fc13c909a21
IronManWSB <61561783+IronManWSB@users.noreply.github.com>
2020-04-21 14:50:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Critical Bank</title>
+</head>
+<body>
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+
+    <div class id="top_menu">
+        <div class="container">
+            <div class="col-md-6 offer">
+                <a href="#" class= "btn btn-success btn-sm">Critical Bank</a>
+                <a href="checkuot.php"> Users is 5</a>
+            </div>
+            <div class="col-md-6">
+                <ul class="menu">
+                    <li>
+                        <a href="Customer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="chechout.php">My account</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="navbar" class="navbar navbar-default">
+        <div class="cointainer">
+
+            <div class="navbar-header">
+                
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <i class="fa fa-align-justify"></i>
+                </button>
+
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                    <span class="sr-only">Toggle Search</span>
+                    <i class="fa fa-search"></i>
+                </button>
+
+            </div>
+
+            <div class="navbar-collapse collapse" id="navigation">
+                <div class="padding-nav">
+                    <ul class="nav navbar-nav left">
+                        <li class="active">
+                            <a href="index.php">Główna</a>
+                        </li>
+                        <li>
+                            <a href="index.php">Moje konto</a>
+                        </li>
+                        <li>
+                            <a href="index.php">Przelewy</a>
+                        </li>
+                        <li>
+                            <a href="index.php">Kontakt</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
