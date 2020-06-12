ee44e99a99646c11e8719f5bf0ec4b9f4c3c1761
yondyy <65918622+yondyy@users.noreply.github.com>
2020-06-12 03:07:23 +0200

    Add files via upload
    style logowania i rejestracji zrobione

--- login.php
+++ login.php
@@ -1,11 +1,27 @@
-
-
-<form action="save_login.php" method="post">
-
-<label for="email">e-mail:</label><br>
-  <input type="email" id="email" name="email" required><br>
-  <label for="Haslo">Hasło:</label><br>
-  <input type="password" id="Haslo" name="Haslo" required><br>
-  <input type="submit" value="Zaloguj">
-
-</form>
+<html lang="pl">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="./styles/login.css">
+      <link rel="stylesheet" href="./styles/bootstrap.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <title>Bank online - Poznań</title>
+  </head>
+  <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+          <h2 class="h2-main" style="text-align:justify">LOGOWANIE DO BANKU</h2>  
+          </div> 
+          <form class="main-form" action="save_login.php" method="post">
+              <label class="main-label" for="email" >E-mail:</label><br>
+                <input class="input-main" type="email" id="email" name="email" maxlength="45" placeholder="Podaj e-mail" required><br>
+              <label class="main-label" style="margin-top:2%" for="Haslo">Hasło:</label><br>
+                <input class="input-main" type="password" id="Haslo" name="Haslo" maxlength="45" placeholder="Podaj hasło" required><br>
+                <input class="submit-button" type="submit" value="Zaloguj">
+          </form>
+      </div>
+        </div>
+          </div>
+  </body>
+</html>
